d6a9d4ffb5657f65098c4847949475bd95c8d4bf
catdgomez <catherinedgomez@gmail.com>
2023-01-04 18:47:09 -0600

    add attributes to db

--- db/migrate/20221227022333_create_locations.rb
+++ db/migrate/20221227022333_create_locations.rb
@@ -1,7 +1,8 @@
 class CreateLocations < ActiveRecord::Migration[6.1]
   def change
     create_table :locations do |t|
-
+      t.string :name
+      t.string :address
       t.timestamps
     end
   end
